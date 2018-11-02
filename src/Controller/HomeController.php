a48f45e08bec03cfcd6ca839492d4942fb604941
Helene Bloch <ln-45@live.fr>
2018-11-02 15:28:22 +0100

    quete symfony 3

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ln
+ * Date: 02/11/18
+ * Time: 15:18
+ */
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/url")
+     */
+    public function index()
+    {
+        return new Response(
+            $this->render('home.html.twig')
+        );
+    }
+}
